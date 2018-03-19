161c33ebd429cb9d4bc926397aef1dba586606d9
rezzonicoda <>
2018-03-19 15:13:51 +0100

    Revert "first commit"
    This reverts commit 8bde6f9c8c0a3056fca4df7651f35e88504d8679.

--- src/main/java/uk/co/flax/biosolr/ontology/core/ols/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * OLS-specifc implementations of the OntologyHelper
- * class.
- *
- * @author Matt Pearce
- */
-package uk.co.flax.biosolr.ontology.core.ols;
--- src/main/java/uk/co/flax/biosolr/ontology/core/owl/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * OWL-specific implementation of the OntologyHelper class.
- *
- * @author Matt Pearce
- */
-package uk.co.flax.biosolr.ontology.core.owl;
